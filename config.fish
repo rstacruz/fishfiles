2d2ce7466c214b6eadbc044a54babc6204383844
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-20 04:16:24 +0800

    Improvements

--- config.fish
+++ config.fish
@@ -3,20 +3,23 @@ set -e fish_greeting
 
 # brew install autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and \
-    . /usr/local/share/autojump/autojump.fish
+   . /usr/local/share/autojump/autojump.fish
 
 # brew install chruby-fish
 # https://github.com/jeanmertz/chruby-fish
 [ -f /usr/local/share/chruby/chruby.fish ]; and \
-    . /usr/local/share/chruby/chruby.fish
+   . /usr/local/share/chruby/chruby.fish
 
 # brew install direnv
 # https://github.com/zimbatm/direnv/
 # which direnv > /dev/null; and \
 #     eval (direnv hook fish)
 
-# theme
+# local files
 . ~/.config/fish/local/theme.fish
+. ~/.config/fish/local/vi_mode.fish
+
+# bins
 set -u fish_user_paths ~/.config/fish/bin $fish_user_paths
 
 # brew install git-extras
@@ -24,12 +27,3 @@ set -u fish_user_paths ~/.config/fish/bin $fish_user_paths
 
 # so tmux won't complain
 export SHELL=/usr/local/bin/fish
-
-# Vi mode, fixing ctrl-c to cancel the line instead of exiting insert mode
-fish_vi_mode
-function fish_vi_key_bindings_local
-  fish_vi_key_bindings
-  bind -e -M insert \cc
-  bind -M insert -m insert \cc 'commandline ""'
-end
-set -g fish_key_bindings fish_vi_key_bindings_local
---
+++ local/vi_mode.fish
@@ -0,0 +1,8 @@
+# Vi mode, fixing ctrl-c to cancel the line instead of exiting insert mode
+fish_vi_mode
+function fish_vi_key_bindings_local
+  fish_vi_key_bindings
+  bind -e -M insert \cc
+  bind -M insert -m insert \cc 'commandline ""'
+end
+set -g fish_key_bindings fish_vi_key_bindings_local
