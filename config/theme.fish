1c0e58e7261d72a769ba465c475f3a64bfc75018
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-21 20:32:19 +0800

    Update upstream characters

--- config/theme.fish
+++ config/theme.fish
@@ -18,8 +18,8 @@ set __fish_git_prompt_char_stagedstate '•'
 set __fish_git_prompt_char_stashstate ''
 set __fish_git_prompt_char_upstream_equal '' #(set_color green)'✓'
 set __fish_git_prompt_char_upstream_prefix ''
-set __fish_git_prompt_char_upstream_ahead ' ↑'
-set __fish_git_prompt_char_upstream_behind ' ↓'
+set __fish_git_prompt_char_upstream_ahead ' ▲'
+set __fish_git_prompt_char_upstream_behind ' ▼'
 set __fish_git_prompt_char_upstream_diverged ' ↔'
 
 function fish_prompt_rsc_vi --description "Displays the current mode"
