a80206488a256992894326d9ff3303bbcab14463
Rico Sta. Cruz <hi@ricostacruz.com>
2015-06-04 19:32:38 +0800

    Update

--- config.fish
+++ config.fish
@@ -6,3 +6,7 @@ set -u fish_user_paths ~/.config/fish/bin $fish_user_paths
 
 # autoload files
 for f in ~/.config/fish/config/*.fish; . $f; end
+
+for f in ~/.config/fish/_packages/*/bin
+  set -u fish_user_paths $fish_user_paths $f
+end
