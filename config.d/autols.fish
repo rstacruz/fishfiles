73be595f4a1fdc122099730fdbee5ec29d32690d
Rico Sta. Cruz <hi@ricostacruz.com>
2015-06-05 00:00:02 +0800

    Add motd

--- config.d/autols.fish
+++ config.d/autols.fish
@@ -1,6 +1,19 @@
 function __autols_hook --description "Auto ls" --on-event fish_prompt
   if test "$__autols_last" != (pwd)
-    echo; ls
+    if test "$HOME" = (pwd)
+      echo
+      echo " →" (set_color --bold) (date +"%I:%M%p")
+      set_color normal
+      set_color black
+      date +"    %a · %B %e"
+      set_color normal
+      echo
+      if test -f ~/.motd
+        cat ~/.motd | sed 's/^/    /g'
+      end
+    else
+      echo; ls
+    end
   end
   set  -g __autols_last (pwd)
 end
