e0b67695a10c43f135ca98078213445196e947a4
Rico Sta. Cruz <hi@ricostacruz.com>
2015-06-04 23:47:34 +0800

    Rename to config.d

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ set -e fish_greeting
 set -u fish_user_paths ~/.config/fish/bin $fish_user_paths
 
 # autoload files
-for f in ~/.config/fish/config/*.fish; . $f; end
+for f in ~/.config/fish/config.d/*.fish; . $f; end
 
 for f in ~/.config/fish/_packages/*/bin
   set -u fish_user_paths $fish_user_paths $f
