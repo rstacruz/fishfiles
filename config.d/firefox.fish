badb0d64956da1cf18a5b74848acaae430a5440a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-07-07 06:40:59 +0800

    Update

---
+++ config.d/firefox.fish
@@ -0,0 +1,3 @@
+if test -d $HOME/Applications/Firefox.app
+  set -x FIREFOX_BIN $HOME/Applications/Firefox.app/Contents/MacOS/firefox-bin
+end
---
+++ config.d/home-env.fish
@@ -0,0 +1,3 @@
+if test -f ~/.env
+  source ~/.env
+end
