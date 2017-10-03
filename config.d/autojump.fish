c632c8403206953bf11c7d61066ddacea6e8ddb3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-10-03 08:45:39 +0800

    Support autojump on Linux

--- config.d/autojump.fish
+++ config.d/autojump.fish
@@ -1,3 +1,7 @@
 # brew install autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and \
    . /usr/local/share/autojump/autojump.fish
+
+# apt install autojump
+[ -f /usr/share/autojump/autojump.fish ]; and \
+   . /usr/share/autojump/autojump.fish
