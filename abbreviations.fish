6752621528d67fe39db1cb804ed6e970ebd7256a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-10-22 03:36:54 +0800

    Update

--- abbreviations.fish
+++ abbreviations.fish
@@ -10,10 +10,7 @@ abbr be 'bundle exec'
 abbr bs 'brew services'
 abbr cask 'brew cask'
 abbr cl clear
-abbr cloc. 'cloc --exclude-dir=vendor,db,log,node_modules,tmp --found=_list.txt .'
 abbr d dirh
-abbr duh 'du -d 1 | sort -n'
-abbr fig docker-compose
 abbr g git
 abbr g. 'git add --all .; and git commit -m Update'
 abbr g.. 'git add --all .; and git commit --amend -m Update; git push --force'
@@ -44,12 +41,10 @@ abbr ixp 'iex -S mix phoenix.server'
 abbr ixs 'iex -S mix'
 abbr j z
 abbr k beep
-abbr k7 'katflix --peerflix="--vlc" --subliminal="--language en"'
 abbr l ls
 abbr m mix
 abbr macdown 'open -a MacDown.app'
 abbr mled 'make -f $HOME/Config/ledger/Makefile'
-abbr mm '~/Config/bin/mm'
 abbr mochaw 'mocha -b -R min --watch'
 abbr ms metalstart
 abbr mt 'mix test'
@@ -68,15 +63,8 @@ abbr sha256 'shasum -a 256'
 abbr srestart 'brew services restart'
 abbr sstart 'brew services start'
 abbr sstop 'brew services stop'
-abbr stash-pub 'git add --all .; git stash; git pull --rebase; git stash pop; npm update; and npm test; and npm publish'
-abbr t task
 abbr tg 'tig grep'
 abbr tigs 'tig status'
 abbr v nvim
-abbr vj 'nvim ~/.journal'
-abbr vl 'nvim $LEDGER_FILE'
 abbr w3c 'curl -H "Content-Type: text/html; charset=utf-8" --data-binary @- "https://validator.w3.org/nu/?out=gnu"'
 abbr y yarn
-abbr ya 'yarn add --exact'
-abbr yad 'yarn add --exact --dev'
-abbr yr 'yarn run'
