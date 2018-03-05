03b7285c3b822338133ba5e6f745e98f52f54ef8
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-05 16:05:08 +0800

    Update abbreviations

--- abbreviations.fish
+++ abbreviations.fish
@@ -1,7 +1,7 @@
 abbr 1 prevd
 abbr 2 nextd
 abbr :E nvim
-abbr :e nvim
+abbr :e 'emacsclient -t'
 abbr :q exit
 abbr ag 'ag --pager "less -R"'
 abbr aoeu asdf
@@ -13,7 +13,9 @@ abbr cask 'brew cask'
 abbr cl clear
 abbr d dirh
 abbr e 'emacsclient -t'
+abbr ec 'emacsclient -nw -t'
 abbr em 'emacsclient -t'
+abbr et 'emacsclient -t'
 abbr g git
 abbr g. 'git add --all .; and git commit -m Update'
 abbr g.. 'git add --all .; and git commit --amend -m Update; git push --force'
@@ -25,6 +27,8 @@ abbr gc 'git commit'
 abbr gcam 'git commit --amend -m'
 abbr gcd 'cd (git rev-parse --show-toplevel)'
 abbr gcm 'git commit -m'
+abbr gco 'git checkout'
+abbr gcot 'git checkout --track'
 abbr gcp 'git cherry-pick'
 abbr gd 'git diff'
 abbr gentr 'git ls-files | entr'
@@ -35,20 +39,12 @@ abbr gfrh 'git fetch; and git reset --hard'
 abbr gg 'git grep --heading --break -p'
 abbr gpr 'git pull --rebase'
 abbr hl hledger
-abbr hla 'grep alias $LEDGER_FILE; hledger add'
-abbr hlac 'hledger accounts --tree'
-abbr hlb 'hledger balance --cleared --cost --empty -e tomorrow --tree --historical'
-abbr hlbs 'hledger balancesheet --cleared --cost --empty -e tomorrow'
-abbr hlr 'hledger reg --cleared --cost --empty -e tomorrow'
 abbr ixp 'iex -S mix phoenix.server'
 abbr ixs 'iex -S mix'
 abbr j z
-abbr k beep
 abbr l ls
 abbr m mix
 abbr macdown 'open -a MacDown.app'
-abbr mled 'make -f $HOME/Config/ledger/Makefile'
-abbr mochaw 'mocha -b -R min --watch'
 abbr ms metalstart
 abbr mt 'mix test'
 abbr mts 'mix test --stale --exclude pending'
@@ -59,6 +55,7 @@ abbr nt 'npm test'
 abbr ntw 'npm test -- --watch'
 abbr nv nvim
 abbr opne open
+abbr org 'emacsclient -t ~/org/notes.org'
 abbr psaux 'ps aux'
 abbr psgrep 'ps aux | grep'
 abbr sai 'sudo apt install'
@@ -66,8 +63,9 @@ abbr sha256 'shasum -a 256'
 abbr srestart 'brew services restart'
 abbr sstart 'brew services start'
 abbr sstop 'brew services stop'
+abbr ta 'tmux attach; or tmux'
 abbr tg 'tig grep'
 abbr tigs 'tig status'
-abbr v nvim
+abbr v 'emacsclient -t'
 abbr w3c 'curl -H "Content-Type: text/html; charset=utf-8" --data-binary @- "https://validator.w3.org/nu/?out=gnu"'
 abbr y yarn
