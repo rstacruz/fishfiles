161ce0f60494a701877bf592209c028836e9f881
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-13 15:54:49 +0800

    Add 'eran' and 'cran' as ranger aliases
    These will launch emacs or code (vscode), respectively.

--- abbreviations/editor.fish
+++ abbreviations/editor.fish
@@ -5,5 +5,8 @@ abbr :E 'emacsclient -t -c -a \'\''
 abbr e 'emacsclient -t -c -a \'\''
 
 # Ranger
+abbr eran    'env EDITOR="emacsclient -t -c -a \'\'" ranger'
 abbr eranger 'env EDITOR="emacsclient -t -c -a \'\'" ranger'
+
+abbr cran    'env EDITOR="code" ranger'
 abbr cranger 'env EDITOR="code" ranger'
