ffb8a165a7b4b84a2dce49b0e05b0fdcad9ac833
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-06-27 08:10:19 +0800

    Update Docker abbreviations for 'docker run'

--- abbreviations/docker.fish
+++ abbreviations/docker.fish
@@ -1,7 +1,9 @@
 abbr dc 'docker-compose'
+
 abbr dcu 'docker-compose up'
 abbr dcx 'docker-compose exec'
 abbr dce 'docker-compose exec'
 abbr dcr 'docker-compose run'
-abbr dx 'docker-compose run --rm --no-deps web'
-abbr dxx 'docker-compose run --rm web'
+
+abbr dr 'docker-compose run --rm web'
+abbr drr 'docker-compose run --rm --no-deps web'
