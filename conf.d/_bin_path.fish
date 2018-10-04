c240a4007e0e7f22a811ff7259dd687cc7baf9c2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-04 22:21:31 +0800

    Improve support for pip3

--- conf.d/_bin_path.fish
+++ conf.d/_bin_path.fish
@@ -1,2 +1,7 @@
 # Add bin/ to the fish user paths
 set -gx fish_user_paths ~/.config/fish/bin $fish_user_paths
+
+# For pip3
+if test -d ~/.local/bin
+  set -gx fish_user_paths ~/.local/bin $fish_user_paths
+end
