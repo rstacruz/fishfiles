1bde34f3c33955a90d56a0249565d4cd65a06a80
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-15 18:20:54 +0800

    Support .mp and .nomp

--- abbreviations/misc.fish
+++ abbreviations/misc.fish
@@ -2,3 +2,7 @@ abbr are 'autorenice'
 
 # Battery
 abbr batt 'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "percent|time"'
+
+# minimal prompt
+abbr .mp 'env MINIMAL_PROMPT=1 fish'
+abbr .nomp 'set -gx MINIMAL_PROMPT'
