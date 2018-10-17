248408cff9da086c4f9e7d28fe1e3f94c6039e73
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-17 17:36:49 +0800

    Update

---
+++ conf.d/_wal_sequences.fish
@@ -0,0 +1,5 @@
+
+# https://github.com/dylanaraps/pywal/wiki/Getting-Started#applying-the-theme-to-new-terminals
+if test -e ~/.cache/wal/sequences; and status --is-interactive
+  cat ~/.cache/wal/sequences &
+end
