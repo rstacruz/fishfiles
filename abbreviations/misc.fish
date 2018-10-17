602dd2580b75abed59aae128b46c6ba5a1c5b9e7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-17 14:35:40 +0800

    Add 'today' abbr

--- abbreviations/misc.fish
+++ abbreviations/misc.fish
@@ -6,3 +6,6 @@ abbr batt 'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "per
 # minimal prompt
 abbr .mp 'env MINIMAL_PROMPT=1 fish'
 abbr .nomp 'set -gx MINIMAL_PROMPT'
+
+# For today (eg, `git checkout -b (today )/update`)
+abbr today "date +'%Y-%m-%d'"
