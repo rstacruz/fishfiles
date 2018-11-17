fda59583c500f74a6cc70da8f2fe4a0ffbfd68ba
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-17 22:23:18 +0800

    .

--- functions/ls!.fish
+++ functions/ls!.fish
@@ -3,7 +3,7 @@ function ls! --description "Auto notes" # --on-event fish_prompt
   set PROJECT_NOTES_PATH "$HOME/org/Projects"
   set project_note $PROJECT_NOTES_PATH"/"$project_name".txt"
 
-  if test -f $project_note
+  if test -f "$project_note"
     echo ""
     set_color -i blue
     cat $project_note | sed 's/^/    /g' | sed -e '/----/,$d'
