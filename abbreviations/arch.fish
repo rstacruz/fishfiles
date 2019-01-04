7db40587815cfff926d947b84acab5f4667db83e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-05 00:42:01 +0800

    Update arch abbrevs

--- abbreviations/arch.fish
+++ abbreviations/arch.fish
@@ -1,9 +1,5 @@
 # Arch Linux abbreviations
 
-abbr yas 'yay -Syu'
-
 abbr spr 'sudo pacman -Rsc' # Remove a package
-
 abbr sys 'sudo systemctl'
-
-abbr flathub 'flatpak --user install flathub'
+abbr pac 'pacman'
