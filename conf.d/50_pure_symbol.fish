f2cc1ac845f17429659b983a6ef33730565beda3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-12 00:32:35 +0800

    Adjust arrow glyphs

--- conf.d/50_pure_symbol.fish
+++ conf.d/50_pure_symbol.fish
@@ -4,6 +4,6 @@ set -gx pure_prompt_begin_with_current_directory false
 # the glyph
 set -gx pure_symbol_prompt "─"
 
-set -gx pure_symbol_git_arrow_down " ↓"
-set -gx pure_symbol_git_arrow_up " ↑"
+set -gx pure_symbol_git_arrow_down "▼ "
+set -gx pure_symbol_git_arrow_up "▲ "
 set -gx pure_symbol_git_dirty " ★"
