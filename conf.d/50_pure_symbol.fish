79904bc38886eb7cf3fb69c4dc8a8e57abae8df6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-13 16:52:45 +0000

    Update ssh colors

--- conf.d/50_pure_symbol.fish
+++ conf.d/50_pure_symbol.fish
@@ -1,5 +1,7 @@
 # ssh hostname first
 set -gx pure_prompt_begin_with_current_directory false
+set -gx pure_color_ssh_user_normal (set_color cyan)
+set -gx pure_color_ssh_user_root (set_color red)
 
 # the glyph
 set -gx pure_symbol_prompt "─"
