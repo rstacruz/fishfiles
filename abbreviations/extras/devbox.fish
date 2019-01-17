f8d62008f0c7d9012a0daec0c60ecc7c02360c11
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-17 11:07:03 +0800

    Add some devbox aliases

---
+++ abbreviations/extras/devbox.fish
@@ -0,0 +1,2 @@
+abbr -a -U -- A 'mosh --experimental-remote-ip=remote rsc@devbox.remote -- sh -c "tmux attach || tmux"'
+abbr -a -U -- N 'mosh --experimental-remote-ip=remote rsc@devbox.remote -- sh -c "nvim NOTES"'
