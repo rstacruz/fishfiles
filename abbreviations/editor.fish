a99af585639840491fbd543c2b2b6d950fddfab2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-23 21:43:14 +0800

    Update

--- abbreviations/editor.fish
+++ abbreviations/editor.fish
@@ -1,36 +1,40 @@
 # Editor
-abbr e  'nvr'
 
-abbr a  'atom-open'
-abbr a. 'atom-open .'
+# [Atom]
+# abbr a  'atom-open'
+# abbr a. 'atom-open .'
 
-# Neovim (server and client)
+# [Neovim]
 abbr v 'nvim'
 abbr n 'nvim'
+abbr :e 'nvim'
+abbr :E 'nvim'
+
+# [Neovim server]
 # abbr v 'nvr -s'
 # abbr v. 'nvr -s --remote-send "<Esc>:cd "(pwd)"<CR>:NERDTree<CR>"'
 
-# Emacs
-abbr ec    'emacsclient -t -c -a \'\''
+# [Emacs]
+# abbr ec    'emacsclient -t -c -a \'\''
 
-# Code
-abbr c     'code -r'
-abbr c.    'code -r.'
+# [Code]
+# abbr c     'code -r'
+# abbr c.    'code -r.'
 
 # Ranger
-abbr ra      'ranger'
+# abbr ra      'ranger'
 
-abbr era     'env EDITOR="emacsclient -t -c -a \'\'" ranger'
-abbr eranger 'env EDITOR="emacsclient -t -c -a \'\'" ranger'
+# abbr era     'env EDITOR="emacsclient -t -c -a \'\'" ranger'
+# abbr eranger 'env EDITOR="emacsclient -t -c -a \'\'" ranger'
 
-abbr cra     'env EDITOR="code" ranger'
-abbr cranger 'env EDITOR="code" ranger'
+# abbr cra     'env EDITOR="code" ranger'
+# abbr cranger 'env EDITOR="code" ranger'
 
-abbr Vra     'env EDITOR="nvim" ranger'
-abbr Vranger 'env EDITOR="nvim" ranger'
+# abbr Vra     'env EDITOR="nvim" ranger'
+# abbr Vranger 'env EDITOR="nvim" ranger'
 
-abbr vra     'env EDITOR="nvr -cc split" ranger'
-abbr vranger 'env EDITOR="nvr -cc split" ranger'
+# abbr vra     'env EDITOR="nvr -cc split" ranger'
+# abbr vranger 'env EDITOR="nvr -cc split" ranger'
 
-abbr ara     'env EDITOR="atom-open" ranger'
-abbr aranger 'env EDITOR="atom-open" ranger'
+# abbr ara     'env EDITOR="atom-open" ranger'
+# abbr aranger 'env EDITOR="atom-open" ranger'
