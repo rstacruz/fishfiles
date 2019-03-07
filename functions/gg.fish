9255ecfe8d2cd6d33acefbe65603e67ce419ce91
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-03-07 17:33:09 +0800

    Update

--- functions/gg.fish
+++ functions/gg.fish
@@ -1,4 +1,10 @@
-function gg
-  rg -p $argv | less -R
+function gg -d "Search via ripgrep/git grep"
+  if type -q rg
+    rg -p $argv | less -R
+  else if type -q ag
+    ag --pager 'less -R' $argv
+  else
+    git grep $argv
+  end
 end
 
