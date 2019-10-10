17e3774d8bb786ccd0b792c29ad2f644e34088c2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-10-10 16:17:21 +1100

    Update code -r abbrevs

--- abbreviations/editor.fish
+++ abbreviations/editor.fish
@@ -12,8 +12,8 @@ end
 if ! type -q code
   echo "(Skipping 'code' abbreviations - vscode not found)"
 else
- abbr c     'code -r (git rev-parse --show-toplevel)'
- abbr C     'code (git rev-parse --show-toplevel)'
+ abbr cr    'code -r (git rev-parse --show-toplevel)'
+ abbr c     'code (git rev-parse --show-toplevel)'
 end
 
 
