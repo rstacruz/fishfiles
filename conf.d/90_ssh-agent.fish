1bbcef4b9f95611d46165983ae014c7b9fb48e35
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-10-12 07:41:12 +1100

    Supress SSH agent on WSL

--- conf.d/90_ssh-agent.fish
+++ conf.d/90_ssh-agent.fish
@@ -1,7 +1,11 @@
-if ! pgrep -u "$USER" ssh-agent > /dev/null
-    ssh-agent -c > "$XDG_RUNTIME_DIR/ssh-agent.env.fish"
-end
+if set -q XDG_RUNTIME_DIR
+    if ! pgrep -u "$USER" ssh-agent > /dev/null
+        ssh-agent -c > "$XDG_RUNTIME_DIR/ssh-agent.env.fish"
+    end
 
-if test -z "$SSH_AUTH_SOCK"
-    eval (cat "$XDG_RUNTIME_DIR/ssh-agent.env.fish") >/dev/null
+    if test -z "$SSH_AUTH_SOCK"
+        if test -f "$XDG_RUNTIME_DIR/ssh-agent.env.fish"
+            eval (cat "$XDG_RUNTIME_DIR/ssh-agent.env.fish") >/dev/null
+        end
+    end
 end
