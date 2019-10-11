b46e1e32f20c4d5da9634eeb07d6fcaaa21704b0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-10-12 07:38:34 +1100

    Update

--- conf.d/90_wsl_x_server.fish
+++ conf.d/90_wsl_x_server.fish
@@ -1,4 +1,6 @@
 if test -f /mnt/c/Windows/explorer.exe
-  export DISPLAY=:0
-  export LIBGL_ALWAYS_INDIRECT=1
+  if ! set -q DISPLAY
+    export DISPLAY=:0
+    export LIBGL_ALWAYS_INDIRECT=1
+  end
 end
