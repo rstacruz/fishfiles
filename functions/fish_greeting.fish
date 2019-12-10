a226d17a5c038a538dce11ba78b4216f7374397f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-12-10 20:08:41 +1100

    Nicer greeting

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,33 +1,42 @@
-function fish_greeting
+function show_battery
     set -l hilite cyan
-    set -l date (date "+%A, %b %e")
-    if type -q toilet
-        echo ""
-        set_color $hilite
-        date +" %I:%M %p " | toilet -f future
-        echo ""
-        echo "  "(set_color brblack)"$date"(set_color normal)
-    else
-        set -l time (date "+%I:%M %p")
-        echo ""
-        echo "  "(set_color cyan)"$time"(set_color normal)
-        echo "  "(set_color normal)"$date"(set_color normal)
-        echo ""
-    end
+    set -l mute brblack
 
     if type -q upower
         set -l perc (upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {print $2}')
         set -l state (upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/state:/ {print $2}')
         if test "$state" = "discharging"
             set -l left (upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/time to empty:/ {print $4, $5}')
-            echo "  "(set_color $hilite)"   $perc"(set_color normal)" · $left left"
+            echo (set_color $hilite)"   $perc"(set_color $mute)" · $left left"
         else if test "$state" = "fully-charged"
-            echo "  "(set_color $hilite)"   100%"
+            echo (set_color $hilite)"   100%"
         else
-            echo "  Battery ($state): $perc"
+            echo (set_color $mute)"Battery ($state): $perc"
         end
         echo ""
     end
 
     set_color normal
+end
+
+function fish_greeting
+    set -l hilite cyan
+    set -l mute brblack
+    set -l date (date "+%A, %b %e")
+    set -l time (date "+%I:%M %p")
+    set -l ind "  "
+    if type -q toilet
+        echo -ne "\033[s"
+        echo ""
+        set_color $hilite
+        date +" %I:%M %p " | toilet -f future --gay
+        set ind "\033[30C"
+        echo -ne "\033[u"
+    end
+    echo ""
+    echo -e $ind(set_color $hilite)"$date"(set_color normal)
+    echo -e $ind(show_battery)
+    echo -e $ind(set_color $mute)"$time"(set_color normal)
+    echo ""
+    echo ""
 end
