2bd3a3232998814994d51f582700ae2beea62ef1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-02-11 01:34:23 +1100

    Set EDITOR to nvr when nesting

--- conf.d/20_editor.fish
+++ conf.d/20_editor.fish
@@ -1,13 +1,4 @@
 if test -n "$NVIM_LISTEN_ADDRESS"
-  set -gx EDITOR "nvr --remote"
-else if test -z "$EDITOR"
-  if which nvim > /dev/null
-    set -gx EDITOR "nvim"
-  else if which vim > /dev/null
-    set -gx EDITOR "vim"
-  else if which emacsclient > /dev/null
-    set -gx EDITOR "emacsclient -t -c -a ''"
-  else if which nano > /dev/null
-    set -gx EDITOR "nano"
-  end
+  set -gx EDITOR "nvr --remote-wait"
+  set -gx VISUAL "nvr --remote-wait"
 end
