53b9a4c288004058a277503f50236605e44e5ce9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-02-11 01:41:31 +1100

    Use remote-tab-wait

--- conf.d/20_editor.fish
+++ conf.d/20_editor.fish
@@ -1,4 +1,4 @@
 if test -n "$NVIM_LISTEN_ADDRESS"
-  set -gx EDITOR "nvr --remote-wait"
-  set -gx VISUAL "nvr --remote-wait"
+  set -gx EDITOR "nvr --remote-tab-wait"
+  set -gx VISUAL "nvr --remote-tab-wait"
 end
