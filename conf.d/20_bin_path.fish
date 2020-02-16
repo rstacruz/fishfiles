73829999b76bae00140bec89440c8c01fdeb3bcc
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-02-17 08:19:33 +1100

    Add ~/bin

--- conf.d/20_bin_path.fish
+++ conf.d/20_bin_path.fish
@@ -3,6 +3,10 @@ if not contains ~/.config/fish/bin $fish_user_paths
   set -gx fish_user_paths ~/.config/fish/bin $fish_user_paths
 end
 
+if not contains ~/bin $fish_user_paths && test -d ~/bin
+  set -gx fish_user_paths ~/bin $fish_user_paths
+end
+
 # For pip3
 # if test -d ~/.local/bin
 #   set -gx fish_user_paths ~/.local/bin $fish_user_paths
