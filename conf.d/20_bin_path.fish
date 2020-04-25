31365ace8363adac614bbe2885376358379b2ac4
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-04-25 14:59:37 +1000

    Add wsl paths (terribly)

--- conf.d/20_bin_path.fish
+++ conf.d/20_bin_path.fish
@@ -7,6 +7,10 @@ if not contains $HOME/bin $PATH && test -d $HOME/bin
   set --prepend PATH $HOME/bin
 end
 
+if not contains $HOME/bin/wsl $PATH && test -d $HOME/bin/wsl && test -d /mnt/c/Windows
+  set --prepend PATH $HOME/bin/wsl
+end
+
 # For pip3
 # if test -d ~/.local/bin
 #   set -gx PATH ~/.local/bin $PATH
