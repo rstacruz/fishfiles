18680c48e2f176e3e4feecffcd3d1fae5bf93203
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-06-11 16:20:55 +1000

    Add optional asdf support

---
+++ conf.d/10_asdf.fish
@@ -0,0 +1,12 @@
+# To install asdf:
+#
+#     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+#     asdf plugin add lua
+#     asdf list all lua
+#     asdf install lua 5.3.5
+#     asdf global lua 5.3.5
+#     lua -v
+#
+if test -f ~/.asdf/asdf.fish
+  source ~/.asdf/asdf.fish
+end
