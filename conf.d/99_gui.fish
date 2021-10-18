f8636bd2450bb467abf84af66b5468b832f586cf
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-10-18 12:01:51 +1100

    Spawn sway on tty1

--- conf.d/99_gui.fish
+++ conf.d/99_gui.fish
@@ -1,10 +1,10 @@
 if test (hostname) = "misamino"
   set TTY1 (tty)
   if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-    exec startx
-  else if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty2"
     set -x MOZ_ENABLE_WAYLAND 1
     set -x QT_QPA_PLATFORM wayland
     exec sway --my-next-gpu-wont-be-nvidia
+    else if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty2"
+    exec startx
   end
 end
