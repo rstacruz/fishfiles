feb5f02832e4c76e0efee570647cbdc897cc791e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-10-30 23:09:58 +1100

    Fix hostname error

--- conf.d/99_gui.fish
+++ conf.d/99_gui.fish
@@ -1,10 +1,18 @@
-if test (hostname) = "misamino"
-  set TTY1 (tty)
-  if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-    set -x MOZ_ENABLE_WAYLAND 1
-    set -x QT_QPA_PLATFORM wayland
-    exec sway --my-next-gpu-wont-be-nvidia
-    else if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty2"
-    exec startx
+function start-gui
+  if ! type -q hostname
+    return
+  end
+
+  if test (hostname) = "misamino"
+    set TTY1 (tty)
+    if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+      set -x MOZ_ENABLE_WAYLAND 1
+      set -x QT_QPA_PLATFORM wayland
+      exec sway --my-next-gpu-wont-be-nvidia
+      else if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty2"
+      exec startx
+    end
   end
 end
+
+start-gui
