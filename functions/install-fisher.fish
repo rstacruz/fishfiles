d4380a6edec6b7e1a27335a6fc23f7a3b06fa40e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-01-21 15:11:06 +1100

    Fix fisher installation cleaning out the plugins

--- functions/install-fisher.fish
+++ functions/install-fisher.fish
@@ -1,4 +1,7 @@
 function install-fisher
   # https://github.com/jorgebucaran/fisher
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+  curl -sL https://git.io/fisher | source
+  and fish -c "fisher install jorgebucaran/fisher"
+  and git checkout fish_plugins
+  and exec fish
 end
