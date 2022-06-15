2d1a2829f22da735bb7759cee188f01fb09734b2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-06-16 09:52:04 +1000

    Update etc/add_profiling.fish

--- etc/add_profiling.fish
+++ etc/add_profiling.fish
@@ -2,7 +2,12 @@
 # Add timestamp printing
 
 function main -a cmd
-  if test "$cmd" = "on"
+  if test "$cmd" = "go"
+    disable
+    enable
+    fish -i -c 'if test -n "$lastts"; echo ""(math ('(datecmd)') - $lastts)ms; end'
+    disable
+  else if test "$cmd" = "on"
     disable
     enable
     echo "Enabled. ✓ Use '"(status filename)" off' to disable."
@@ -16,16 +21,20 @@ end
 
 function datecmd
   if type -q gdate
-    echo "gdate"
+    echo "gdate +'%s%3N'"
   else
-    echo "date"
+    echo "date +'%s%3N'"
   end
 end
+
 
 function enable
   cd $HOME/.config/fish/conf.d
   for fn in *.fish
-    echo "echo ("(datecmd)" +"%T.%3N") (status -f) # for profiling" > $fn.tmp
+    echo '# for profiling' > $fn.tmp
+    echo 'if test -n "$lastts"; echo ""(math ('(datecmd)') - $lastts)ms; end # for profiling' >> $fn.tmp
+    echo 'printf "%-30s" (basename (status -f)) # for profiling' >> $fn.tmp
+    echo 'set lastts ('(datecmd)') # for profiling' >> $fn.tmp
     cat $fn >> $fn.tmp
     mv $fn.tmp $fn
   end
