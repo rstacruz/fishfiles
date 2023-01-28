2aa9c5b9be64d0811c5ab8541e28ea54fe81ed2a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-01-28 13:07:18 +1100

    Update functions/install-fisher.fish

--- functions/install-fisher.fish
+++ functions/install-fisher.fish
@@ -1,8 +1,10 @@
 function install-fisher
+  cd $HOME/.config/fish
   # https://github.com/jorgebucaran/fisher
   curl -sL https://git.io/fisher | source
   and fisher install jorgebucaran/fisher
   and git checkout fish_plugins
   and fisher update
+  and fish -c update-settings
   and exec fish
 end
