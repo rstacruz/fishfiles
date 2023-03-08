30249a40bf2c97dfb1c8ead7c32ab456bbe06d7e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-03-08 20:55:33 +1100

    Add gnome-dark and gnome-light

--- functions/update-abbreviations.fish
+++ functions/update-abbreviations.fish
@@ -140,6 +140,11 @@ function update-abbreviations
     abbr prlist 'gh pr list --search "-is:draft"'
   end
 
+  if type -q dconf
+    abbr gnome-dark "gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'"
+    abbr gnome-light "gsettings set org.gnome.desktop.interface color-scheme 'default'"
+  end
+
   # if type -q nvim
   #   # echo "✓ Neovim"
   #   abbr v 'nvim'
