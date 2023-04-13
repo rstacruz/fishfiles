fbf1f7489174c2db62c880a31e34c77cb548b7d6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-04-13 10:36:30 +1000

    Update 20_wal_sequences.fish

--- conf.d/20_wal_sequences.fish
+++ conf.d/20_wal_sequences.fish
@@ -1,9 +1,18 @@
 
 # https://github.com/dylanaraps/pywal/wiki/Getting-Started#applying-the-theme-to-new-terminals
-if test -e ~/.cache/wal/sequences; and status --is-interactive; and test "$USE_WAL" != 0; and test "$TERM_PROGRAM" != vscode; and test "$TERM_PROGRAM" != WezTerm; and test "$SSH_TTY" != ""; # not when SSH'ing remotely \
+if test -e ~/.cache/wal/sequences
+    and status --is-interactive
+    and test "$USE_WAL" != 0
+    and test "$TERM_PROGRAM" != vscode
+    and test "$TERM_PROGRAM" != WezTerm
+    and test "$SSH_TTY" != "" # not when SSH'ing remotely
     and test "$WSLENV" != "" # running wsl.exe in vscode
 
-    if test "$TERM" = rxvt-unicode; or test "$TERM" = xterm-256color; or test "$TERM" = xterm-kitty; or test "$TERM" = alacritty; or test "$USE_WAL" = 1
+    if test "$TERM" = rxvt-unicode
+        or test "$TERM" = xterm-256color
+        or test "$TERM" = xterm-kitty
+        or test "$TERM" = alacritty
+        or test "$USE_WAL" = 1
         cat ~/.cache/wal/sequences &
     end
 end
